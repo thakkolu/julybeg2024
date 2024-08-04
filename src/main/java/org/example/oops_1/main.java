5161ac3beb64157364fea7231309b888f7f8f0d0
thakkolu sravani <tsravani912@gmail.com>
2024-08-04 10:49:09 +0530

    Intro to oops

---
+++ src/main/java/org/example/oops_1/main.java
@@ -0,0 +1,26 @@
+package org.example.oops_1;
+
+public class main {
+    public static void main(String[] args) {
+        student s1=new student();
+        s1.name="sravani";
+        s1.age=25;
+        s1.psp=96.7f;
+        s1.changebatch();
+        s1.joinclass();
+        s1.printdetails();
+
+
+        student s2=new student();
+        s2.name="reddy";
+        s2.age=25;
+        s2.psp=67f;
+        s2.changebatch();
+        s2.joinclass();
+        s2.printdetails();
+
+    }
+
+
+
+}
---
+++ src/main/java/org/example/oops_1/student.java
@@ -0,0 +1,18 @@
+package org.example.oops_1;
+
+public class student {
+    int age;
+    String name;
+    float psp;
+
+    void changebatch(){
+        System.out.println("changind batch");
+    }
+    void joinclass(){
+        System.out.println(name+"is new to this batch");
+    }
+    void printdetails(){
+        System.out.println("name is " + name);
+        System.out.println("age is " +age);
+    }
+}
